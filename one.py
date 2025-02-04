152631245e241433afce82c225eef2dc5281e137
Anjaligagneja07 <anjaligagneja93@gmail.com>
2025-02-04 15:49:18 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,136 @@
+# empty list
+# a=[]
+# print(a)
+# print(type(a))
+# print(45)
+# print(456)
+# indxing
+# slicing
+
+
+
+# a=[4,5,7,4,3,6,8,"king",98.67,True]
+
+# methods
+# b=[2,5,7,3,6,3]
+# c=b
+# print(c)
+# c=b.copy()
+# print(c)
+# b.clear()
+# b.sort(reverse=True)
+# print(b)
+
+# a.reverse() #reverse all 
+# a.remove(4) #specific value => first occur
+# a.pop(0) #specific index ki value delete
+# a.insert(2,88) #specific index par value add 
+# print(a.index(5)) #element => index 
+# a.extend([11,22,33]) #add more than one value in the list in the last => one list into another
+# print(a.count(4)) #counting no of occurance
+# a.append(99)  #add a single value in the list in the last
+# print(a)
+
+# a=[4,5,8,6,2,3,8]
+# print(a)
+# print(type(a))
+# print(a[3])
+# print(a[2:6:1])
+# a.append(79)
+# print(a.count(8))
+# a.clear()
+# a.extend([2,5,1])
+# print(a.index(5))
+# a.insert(3,99)
+# a.pop(1)
+# a.remove(8)
+# a.reverse()
+# a.sort
+# print(a)
+
+
+# a=[4,5,6,2,3,1,2]
+# a.reverse()
+# print(a)
+
+# BOOK PUBLISHER
+# def again():
+#     print("do you want to continue")
+#     print("1 for continue")
+#     print("2 for exit")
+#     b=int(input("enter no."))
+#     if(b==1):
+#         happy()
+#     if(b==2):
+#         print("Thankyou")
+
+# def happy():
+    # print("press 1 for 101 book info")
+    # print("press 2 for 102 book info")
+    # print("press 3 for 103 book info")
+    # print("press 4 for 104 book info")
+    # print("print 5 for 105 book info")
+    # a=int(input("enter no."))#3
+    # if(a==1):
+    #     print(101,"joy of living","benji",248,500)
+    #     again()
+    # if(a==2):
+    #     print(102,"alchemist","joey",230,300)
+    #     again()
+    # if(a==3):
+    #     print(103,"ikigai","ruskin",250,400)
+    #     again()
+    # if(a==4):
+    #     print(104,"psychology of money","rama",150,200)
+    #     again()
+    # if(a==5):
+    #     print(105,"happy","sherlock",250,264)
+    #     again()
+
+# happy()
+
+# JUNGLE GAME
+# def again():
+#     print("do you want to play again?")
+#     print("1 for yes")
+#     print("2 for no")
+#     e=int(input("enter no."))
+#     if(e==1):
+#         happy()
+#     if(e==2):
+#         print("exit")
+# def happy():
+#     print("press 1 for right")
+#     print("press 2 for left")
+#     a=int(input("enter no."))
+#     if(a==1):
+#         print("enter 1 for right")
+#         print("enter 2 for left")
+#         b=int(input("enter no."))
+#         if(b==1):
+#             print("gameover")
+#         if(b==2):
+#             print("animals")
+#             print("gameover")
+#     if(a==2):
+#         print("enter 1 for right")
+#         print("enter 2 for left")
+#         c=int(input("enter no."))
+#         if(c==1):
+#             print("press 1 for help")
+#             print("press 2 for no help")
+#             d=int(input("enter no."))
+#             if(d==1):
+#                 print("win")
+#                 again()
+#             if(d==2):
+#                 print("lose")
+#                 again()
+#         if(c==2):
+#             print("gameover")  
+# happy ()    
+
+        
+
+
+
